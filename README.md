25f4737b2a226b22cc9a4e887caf818ad4a1020e
IraqLobst3r <philippschlinge@protonmail.com>
2020-11-13 11:47:52 +0100

    minor changes

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ On Raspberry pi
 export ROS_MASTER_URI=http://[your-desktop-machine-ip]:11311
 export ROS_IP=[your-desktop-machine-ip]
 
+start rpliidar
+roslaunch rplidar_ros rplidar.launch
+
+start picam
+roslaunch raspicam_node camerav2_1280x960.launch
+
+start joystick
+rosrun joy joy_node
--- rc_car_pi/scripts/servo_contol.py
+++ rc_car_pi/scripts/servo_contol.py
@@ -10,20 +10,21 @@ speed_neutral = 400
 
 def joy_callback(msg):
 
-    current_steering = (msg.axes[0] * 225) + steering_default
-    current_speed = ((-(msg.axes[4] - 1) / 2) * 200 + ((msg.axes[5] -1) / 2) * 200) + speed_neutral
+    current_steering = -(msg.axes[0] * 225) + steering_default
+    current_speed = ((-(msg.axes[5] - 1) / 2) * 200 + ((msg.axes[4] -1) / 2) * 200) + speed_neutral
+    print("set joy")
+    pwm.set_pwm(0, 0, int(current_speed))
+    pwm.set_pwm(1, 0, int(current_steering))
 
-    pwm.set_pwm(0, 0, current_speed)
-    pwm.set_pwm(1, 0, current_steering)
 
 if __name__ == '__main__':
     rospy.init_node('servo_control')
-    rospy.Subscriber('joy', Joy, joy_callback)
-    
+    rospy.Subscriber('/joy', Joy, joy_callback)
+    print("starte servo control")    
     pwm = Adafruit_PCA9685.PCA9685(address=0x40)
     pwm.set_pwm_freq(60)
 
-    pwm.set_pwm(0, 0, speed_neutral)
-    pwm.set_pwm(1, 0, steering_default)
+    pwm.set_pwm(0, 0, int(speed_neutral))
+    pwm.set_pwm(1, 0, int(steering_default))
 
     rospy.spin()

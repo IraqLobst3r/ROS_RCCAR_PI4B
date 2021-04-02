29011aabad37b7f405add5973ef8a65146b2eb58
IraqLobst3r <philippschlinge@protonmail.com>
2021-04-02 14:49:52 +0200

    pi_camera_v2 update

--- src/raspberry_pi_zero/pi_camera_v2/src/pi_camera_v2.cpp
+++ src/raspberry_pi_zero/pi_camera_v2/src/pi_camera_v2.cpp
@@ -19,6 +19,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
+#define GET_VAR_NAME(name) (#name)
+
 using namespace std::chrono_literals;
 
 static const char DEVICE[] = "/dev/video0";
@@ -27,100 +29,42 @@ class PiCamera : public rclcpp::Node {
    public:
     PiCamera() : Node("pi_camera") {
         publisher_ =
-            this->create_publisher<std_msgs::msg::String>("camera_caps", 10);
-        timer_ = this->create_wall_timer(
-            1000ms, std::bind(&PiCamera::timer_callback, this));
-
-        // open camera file
-        fd_ = open(DEVICE, O_RDWR);
-        if (fd_ < 0) {
-            perror(DEVICE);
-        }
-        cropcaps.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+            this->create_publisher<std_msgs::msg::String>("camera_image", 10);
         // init device
         fmtdesc.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
         fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
         // init mmap
         reqbuf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-
-        this->print_caps();
-        this->print_available_formats();
-
-        this->init_device();
-        this->init_mmap();
+        reqbuf.memory = V4L2_MEMORY_MMAP;
+        reqbuf.count = 5;
     }
 
-    ~PiCamera() { close(fd_); }
-
-   private:
-    int init_device() {
-        fmtdesc.index = fmt_index_;
-        ret_ = ioctl(fd_, VIDIOC_ENUM_FMT, &fmtdesc);
-        if (ret_ == -1) {
-            perror("Init Device Format:");
-            return errno;
+    void init_device() {
+        // open camera file
+        fd_ = open(DEVICE, O_RDWR);
+        if (fd_ < 0) {
+            perror(DEVICE);
+            throw;
         }
-        printf("\nUsing format: %s\n", fmtdesc.description);
+
+        // get format
+        fmtdesc.index = fmt_index_;
+        this->xioctl(fd_, VIDIOC_ENUM_FMT, &fmtdesc);
 
         fmt.fmt.pix.width = width_;
         fmt.fmt.pix.height = height_;
         fmt.fmt.pix.pixelformat = fmtdesc.pixelformat;
         fmt.fmt.pix.field = V4L2_FIELD_NONE;
 
-        ret_ = ioctl(fd_, VIDIOC_S_FMT, &fmt);
-        if (ret_ == -1) {
-            perror("Set Format:");
-            return -1;
-        }
+        this->xioctl(fd_, VIDIOC_S_FMT, &fmt);
         this->print_used_format();
-
-        return 0;
+        this->init_mmap();
     }
 
-    int init_mmap() {
-        reqbuf.memory = V4L2_MEMORY_MMAP;
-        reqbuf.count = 5;
-        ret_ = ioctl(fd_, VIDIOC_REQBUFS, &reqbuf);
-        if (ret_ == -1) {
-            perror("VIDIOC_REQBUFS:");
-            return errno;
-        }
-        if (reqbuf.count < 2) {
-            printf("Not enough buffer memory\n");
-            return -1;
-        }
-        buffers = (struct buf*)calloc(reqbuf.count, sizeof(*buffers));
-        assert(buffers != NULL);
-
-        num_buffers_ = reqbuf.count;
-        printf("MMAP Buffer number: %d", num_buffers_);
-        for (unsigned int i = 0; i < reqbuf.count; i++) {
-            memset(&buffer, 0, sizeof(buffer));
-            buffer.type = reqbuf.type;
-            buffer.memory = V4L2_MEMORY_MMAP;
-            buffer.index = i;
-
-            ret_ = ioctl(fd_, VIDIOC_QUERYBUF, &buffer);
-            if (ret_ == -1) {
-                perror("VIDIOC_QUERYBUF:");
-                return errno;
-            }
-
-            buffers[i].length = buffer.length;
-            buffers[i].start = mmap(NULL, buffer.length, PROT_READ | PROT_WRITE,
-                                    MAP_SHARED, fd_, buffer.m.offset);
-
-            if (MAP_FAILED == buffers[i].start) {
-                perror("mmap");
-                return errno;
-            }
-        }
-        return 0;
-    }
-
-    int start_capturing() {
+    void start_capturing() {
         enum v4l2_buf_type type;
 
+        struct v4l2_buffer buffer;
         for (unsigned int i = 0; i < num_buffers_; i++) {
             // Note that we set bytesused = 0, which will set it to the buffer
             // length
@@ -130,21 +74,149 @@ class PiCamera : public rclcpp::Node {
             buffer.index = i;
 
             // Enqueue the buffer with VIDIOC_QBUF
-            ret_ = ioctl(fd_, VIDIOC_QBUF, &buffer);
-            if (ret_ == -1) {
-                perror("VIDIOC_QBUF");
-                return errno;
-            }
+            this->xioctl(fd_, VIDIOC_QBUF, &buffer);
         }
 
         type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        this->xioctl(fd_, VIDIOC_STREAMON, &type);
 
-        ret_ = ioctl(fd_, VIDIOC_STREAMON, &type);
-        if (ret_ == -1) {
-            perror("VIDIOC_STREAMON");
-            return errno;
+        this->loop();
+    }
+
+    void print_caps() {
+        this->xioctl(fd_, VIDIOC_QUERYCAP, &caps);
+        printf(
+            "Driver Caps:\n"
+            "  Driver: \"%s\"\n"
+            "  Card: \"%s\"\n"
+            "  Bus: \"%s\"\n"
+            "  Version: %u.%u.%u\n"
+            "  Capabilities: %08x\n",
+            caps.driver, caps.card, caps.bus_info, (caps.version >> 16) & 0xFF,
+            (caps.version >> 8) & 0xFF, (caps.version) & 0XFF,
+            caps.capabilities);
+    }
+
+    void print_available_formats() {
+        // These will correspond to the flags V4L2_FMT_FLAG_COMPRESSED and
+        // V4L2_FMT_FLAG_EMULATED
+        char c, e;
+
+        while (0 == ioctl(fd_, VIDIOC_ENUM_FMT, &fmtdesc)) {
+            c = fmtdesc.flags & 1 ? 'C' : ' ';
+            e = fmtdesc.flags & 2 ? 'E' : ' ';
+
+            printf("%2d %c%c %s\n", fmtdesc.index, c, e, fmtdesc.description);
+            fmtdesc.index++;
         }
-        return 0;
+    }
+
+    ~PiCamera() {
+        printf("Deconstructor PiCamera");
+        enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+
+        this->xioctl(fd_, VIDIOC_STREAMOFF, &type);
+        // Cleanup
+        for (unsigned int i = 0; i < reqbuf.count; i++)
+            munmap(buffers[i].start, buffers[i].length);
+        free(buffers);
+        close(fd_);
+    }
+
+   private:
+    void init_mmap() {
+        this->xioctl(fd_, VIDIOC_REQBUFS, &reqbuf);
+        if (reqbuf.count < 2) {
+            printf("Not enough buffer memory\n");
+            throw;
+        }
+        buffers = (struct buf*)calloc(reqbuf.count, sizeof(*buffers));
+        assert(buffers != NULL);
+
+        num_buffers_ = reqbuf.count;
+        printf("MMAP Buffer number: %d", num_buffers_);
+
+        struct v4l2_buffer buffer;
+        for (unsigned int i = 0; i < reqbuf.count; i++) {
+            memset(&buffer, 0, sizeof(buffer));
+            buffer.type = reqbuf.type;
+            buffer.memory = V4L2_MEMORY_MMAP;
+            buffer.index = i;
+
+            this->xioctl(fd_, VIDIOC_QUERYBUF, &buffer);
+
+            buffers[i].length = buffer.length;
+            buffers[i].start = mmap(NULL, buffer.length, PROT_READ | PROT_WRITE,
+                                    MAP_SHARED, fd_, buffer.m.offset);
+
+            if (MAP_FAILED == buffers[i].start) {
+                perror("mmap");
+                throw;
+            }
+        }
+    }
+
+    void loop() {
+        fd_set fds;
+        struct timeval tv;
+        int r;
+        while (true) {
+            // Clear the set of file descriptors to monitor, then add the fd
+            // for our device
+            FD_ZERO(&fds);
+            FD_SET(fd_, &fds);
+
+            // Set the timeout
+            tv.tv_sec = 2;
+            tv.tv_usec = 0;
+
+            r = select(fd_ + 1, &fds, NULL, NULL, &tv);
+
+            if (-1 == r && errno == EINTR) {
+                continue;
+            } else if (r == -1) {
+                throw;
+            } else if (r == 0) {
+                fprintf(stderr, "select timeout\n");
+                throw;
+            } else {
+                this->read_frame();
+            }
+        }
+    }
+
+    int read_frame() {
+        struct v4l2_buffer buffer;
+        memset(&buffer, 0, sizeof(buffer));
+        buffer.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buffer.memory = V4L2_MEMORY_MMAP;
+
+        // Dequeue a buffer
+        if (-1 == ioctl(fd_, VIDIOC_DQBUF, &buffer)) {
+            switch (errno) {
+                case EAGAIN:
+                    // No buffer in the outgoing queue
+                    return 0;
+                case EIO:
+                    // fall through
+                default:
+                    perror("VIDIOC_DQBUF");
+                    throw;
+            }
+        }
+
+        assert(buffer.index < num_buffers_);
+        process_image(buffers[buffer.index].start);
+
+        // Enqueue the buffer again
+        this->xioctl(fd_, VIDIOC_QBUF, &buffer);
+        return 1;
+    }
+
+    void process_image(const void* pBuffer) {
+        this->publisher_->publish("Hello");
+        fputc('.', stdout);
+        fflush(stdout);
     }
 
     void print_used_format() {
@@ -160,66 +232,18 @@ class PiCamera : public rclcpp::Node {
             fmt.fmt.pix.field);
     }
 
-    int print_caps() {
-        ret_ = ioctl(fd_, VIDIOC_QUERYCAP, &caps);
+    void xioctl(int fd, int request, void* arg) {
+        int ret_;
+        do {
+            ret_ = ioctl(fd, request, arg);
+            // retry when write/read operation was interrupted
+        } while ((ret_ == -1) && (errno == EINTR));
+
+        // error has occurred
         if (ret_ == -1) {
-            perror("Querying device capabilities");
-            return errno;
+            perror("ERROR");
+            throw;
         }
-
-        RCLCPP_INFO(this->get_logger(),
-                    "Driver Caps:\n"
-                    "  Driver: \"%s\"\n"
-                    "  Card: \"%s\"\n"
-                    "  Bus: \"%s\"\n"
-                    "  Version: %u.%u.%u\n"
-                    "  Capabilities: %08x\n",
-                    caps.driver, caps.card, caps.bus_info,
-                    (caps.version >> 16) & 0xFF, (caps.version >> 8) & 0xFF,
-                    (caps.version) & 0XFF, caps.capabilities);
-        return 0;
-    }
-
-    int print_crops() {
-        ret_ = ioctl(fd_, VIDIOC_CROPCAP, &cropcaps);
-        if (ret_ == -1) {
-            perror("Querying device crop capabilities");
-            return errno;
-        }
-        RCLCPP_INFO(this->get_logger(),
-                    "Camera Cropping:\n"
-                    "  Bounds: %dx%d+%d+%d\n"
-                    "  Default: %dx%d+%d+%d\n"
-                    "  Aspect: %d/%d\n",
-                    cropcaps.bounds.width, cropcaps.bounds.height,
-                    cropcaps.bounds.left, cropcaps.bounds.top,
-                    cropcaps.defrect.width, cropcaps.defrect.height,
-                    cropcaps.defrect.left, cropcaps.defrect.top,
-                    cropcaps.pixelaspect.numerator,
-                    cropcaps.pixelaspect.denominator);
-        return 0;
-    }
-
-    int print_available_formats() {
-        // These will correspond to the flags V4L2_FMT_FLAG_COMPRESSED and
-        // V4L2_FMT_FLAG_EMULATED
-        char c, e;
-
-        while (0 == ioctl(fd_, VIDIOC_ENUM_FMT, &fmtdesc)) {
-            c = fmtdesc.flags & 1 ? 'C' : ' ';
-            e = fmtdesc.flags & 2 ? 'E' : ' ';
-
-            printf("%2d %c%c %s\n", fmtdesc.index, c, e, fmtdesc.description);
-            fmtdesc.index++;
-        }
-
-        return 0;
-    }
-
-    void timer_callback() {
-        auto message = std_msgs::msg::String();
-        message.data = "Hello";
-        publisher_->publish(message);
     }
 
     struct buf {
@@ -228,27 +252,34 @@ class PiCamera : public rclcpp::Node {
     };
     struct buf* buffers = NULL;
 
-    rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+
     int fd_;
-    int ret_;
     unsigned int num_buffers_;
     int width_ = 1920;
     int height_ = 1080;
     int fmt_index_ = 4;
-    struct v4l2_capability caps = {0};
-    struct v4l2_cropcap cropcaps = {0};
+
+    struct v4l2_capability caps = {};
+    struct v4l2_cropcap cropcaps = {};
     // init device
-    struct v4l2_fmtdesc fmtdesc = {0};
-    struct v4l2_format fmt = {0};
+    struct v4l2_fmtdesc fmtdesc = {};
+    struct v4l2_format fmt = {};
     // init mmap
-    struct v4l2_requestbuffers reqbuf = {0};
-    struct v4l2_buffer buffer;
+    struct v4l2_requestbuffers reqbuf = {};
 };
 
 int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<PiCamera>());
+    auto cam_node = rclcpp::Node::make_shared("pi_camera");
+
+    try {
+        cam_node->init_device();
+        cam_node->start_capturing();
+    } catch (...) {
+        printf("catch block");
+    }
+    printf("shutdown node");
     rclcpp::shutdown();
 
     return 0;

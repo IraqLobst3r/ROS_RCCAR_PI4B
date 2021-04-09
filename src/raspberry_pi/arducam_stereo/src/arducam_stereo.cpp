2640dde5ee52346056dbac978309754d0e398323
IraqLobst3r <philippschlinge@protonmail.com>
2021-04-09 11:03:22 +0200

    arducam

--- src/raspberry_pi/arducam_stereo/src/arducam_stereo.cpp
+++ src/raspberry_pi/arducam_stereo/src/arducam_stereo.cpp
@@ -1,10 +1,28 @@
-#include <cstdio>
+#include <iostream>
+#include <linux/v4l2-controls.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/xphoto/white_balance.hpp>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <vector>
 
-int main(int argc, char ** argv)
-{
-  (void) argc;
-  (void) argv;
+#include "raspicam_stereo/arducam_mipicamera.h"
 
-  printf("hello world arducam_stereo package\n");
-  return 0;
+#include "custom_interfaces/msg/h264_image.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+#define VCOS_ALIGN_DOWN(p, n) (((ptrdiff_t)(p)) & ~((n)-1))
+#define VCOS_ALIGN_UP(p, n) VCOS_ALIGN_DOWN((ptrdiff_t)(p) + (n)-1, (n))
+
+int main(int argc, char** argv) {
+    (void)argc;
+    (void)argv;
+
+    printf("hello world arducam_stereo package\n");
+    return 0;
 }

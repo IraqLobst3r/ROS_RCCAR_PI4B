dd9c0d82cc6b42f83662c2e4ef2e73ac25e517e8
IraqLobst3r <philippschlinge@protonmail.com>
2021-04-10 20:05:37 +0200

    arducam

--- src/raspberry_pi/arducam_stereo/src/arducam_stereo.cpp
+++ src/raspberry_pi/arducam_stereo/src/arducam_stereo.cpp
@@ -23,6 +23,8 @@
 #define VCOS_ALIGN_UP(p, n) VCOS_ALIGN_DOWN((ptrdiff_t)(p) + (n)-1, (n))
 
 int nFrames_ = 0;
+int64 t0 = 0;
+int64 processingTime = 0;
 rclcpp::Node::SharedPtr arducam{nullptr};
 rclcpp::Publisher<custom_interfaces::msg::H264Image>::SharedPtr publisher_{nullptr};
 std::string frame_id_ = "arducam";
@@ -43,21 +45,39 @@ int video_callback(BUFFER* buffer) {
         } else {
             // MMAL_BUFFER_HEADER_FLAG_KEYFRAME
             // MMAL_BUFFER_HEADER_FLAG_FRAME_END
-            if (nFrames_ % 100 == 0) {
-                RCLCPP_INFO(arducam->get_logger(), "send Frames");
-            }
+            custom_interfaces::msg::H264Image h264_msg;
+            h264_msg.header.frame_id = frame_id_;
+            h264_msg.seq = nFrames_++;
 
-            nFrames_++;
-            /* custom_interfaces::msg::H264Image h264_msg; */
-            /* h264_msg.header.frame_id = frame_id_; */
-            /* h264_msg.seq = nFrames_++; */
+            if (nFrames_ % 10 == 0) {
+                const int N = 10;
+                int64 t1 = cv::getTickCount();
+                std::cout << "Frames captured: " << cv::format("%5lld", (long long int)nFrames_)
+                          << "    Average FPS: "
+                          << cv::format("%9.1f", (double)cv::getTickFrequency() * N / (t1 - t0))
+                          << "    Average time per frame: "
+                          << cv::format("%9.2f ms",
+                                        (double)(t1 - t0) * 1000.0f / (N * cv::getTickFrequency()))
+                          << "    Average processing time: "
+                          << cv::format("%9.2f ms", (double)(processingTime)*1000.0f /
+                                                        (N * cv::getTickFrequency()))
+                          << std::endl;
+                t0 = t1;
+                processingTime = 0;
+            }
+            int64 tp0 = cv::getTickCount();
+            /* h264_msg.header.stamp = arducam->now(); */
+            h264_msg.data.insert(h264_msg.data.begin(), buffer->data,
+                                 (buffer->data + buffer->length));
+            publisher_->publish(h264_msg);
 
             /* if (publisher_->get_subscription_count() > 0) { */
-            /*     h264_msg.data.insert(h264_msg.data.begin(), &buffer->data[0], */
-            /*                          &buffer->data[buffer->length]); */
+            /*     h264_msg.data.insert(h264_msg.data.begin(), buffer->data, */
+            /*                          (buffer->data + buffer->length)); */
             /*     h264_msg.header.stamp = arducam->now(); */
             /*     publisher_->publish(h264_msg); */
             /* } */
+            processingTime += cv::getTickCount() - tp0;
             // Here may be just a part of the data, we need to check it.
             if (buffer->flags & MMAL_BUFFER_HEADER_FLAG_FRAME_END) {
             }
@@ -90,125 +110,6 @@ static void default_status(VIDEO_ENCODER_STATE* state) {
     /**********************H264 only**************************************/
 }
 
-/* class ArducamStereo : public rclcpp::Node { */
-/*   public: */
-/*     ArducamStereo() : Node("arducam_stereo") { */
-/*         publisher_ = this->create_publisher<custom_interfaces::msg::H264Image>( */
-/*             "arducam_stereo/h264_image", 10); */
-
-/*         // Available formats: */
-/*         // mode: 7, 1600x600 */
-/*         // mode: 8, 2560x720 */
-/*         // mode: 9, 3840x1080 */
-/*         // mode: 10, 5184x1944 */
-/*         // mode: 11, 6528x1848 */
-/*         // mode: 12, 6528x2464 */
-
-/*         this->declare_parameter<int>("node_rate_loop", 30); */
-/*         this->declare_parameter<int>("width", 2560); */
-/*         this->declare_parameter<int>("height", 720); */
-/*         this->declare_parameter<int>("scale", 2); */
-/*         this->declare_parameter<bool>("auto_exposure", true); */
-/*         this->declare_parameter<int>("exposure_value", 5); */
-/*         this->declare_parameter<bool>("auto_white_balance", true); */
-/*         this->declare_parameter<bool>("auto_wb_compensation", true); */
-/*         this->declare_parameter<int>("red_gain", 10); */
-/*         this->declare_parameter<int>("blue_gain", 50); */
-
-/*         this->get_parameter("node_rate_loop", _node_rate_loop); */
-/*         this->get_parameter("width", _width); */
-/*         this->get_parameter("height", _height); */
-/*         this->get_parameter("scale", _scale); */
-/*         this->get_parameter("auto_exposure", _auto_exposure); */
-/*         this->get_parameter("exposure_value", _exposure_value); */
-/*         this->get_parameter("auto_white_balance", _auto_white_balance); */
-/*         this->get_parameter("auto_wb_compensation", _auto_wb_compensation); */
-/*         this->get_parameter("red_gain", _red_gain); */
-/*         this->get_parameter("blue_gain", _blue_gain); */
-
-/*         RCLCPP_INFO(this->get_logger(), "Opening camera..."); */
-/*         res = arducam_init_camera(&camera_instance); */
-/*         if (res) { */
-/*             RCLCPP_INFO(this->get_logger(), "init camera status = %d", res); */
-/*             throw std::runtime_error("Could not init camera"); */
-/*         } */
-/*         RCLCPP_INFO(this->get_logger(), "Setting resolution..."); */
-/*         res = arducam_set_resolution(camera_instance, &_width, &_height); */
-/*         if (res) { */
-/*             RCLCPP_INFO(this->get_logger(), "set resolution status = %d", res); */
-/*             throw std::runtime_error("Could not set resolution"); */
-/*         } else { */
-/*             RCLCPP_INFO(this->get_logger(), "current resolution is %dx%d", _width, _height); */
-/*         } */
-/*         if (_auto_exposure) { */
-/*             RCLCPP_INFO(this->get_logger(), "Enabling Auto Exposure..."); */
-/*             arducam_software_auto_exposure(camera_instance, 1); */
-/*         } else { */
-/*             if (arducam_set_control(camera_instance, V4L2_CID_EXPOSURE, */
-/*                                     (int)(_exposure_value * 0xFFFF / 200.0))) { */
-/*                 RCLCPP_INFO(this->get_logger(), */
-/*                             "Failed to set exposure, the camera may not support this control.");
- */
-/*             } */
-/*         } */
-/*         if (_auto_white_balance) { */
-/*             RCLCPP_INFO(this->get_logger(), "Enable Auto White Balance..."); */
-/*             if (arducam_software_auto_white_balance(camera_instance, 1)) { */
-/*                 RCLCPP_INFO(this->get_logger(), "Automatic white balance not supported"); */
-/*             } */
-/*         } */
-/*         if (_auto_wb_compensation) { */
-/*             RCLCPP_INFO(this->get_logger(), "Enable Auto White Balance Compensation..."); */
-/*             arducam_manual_set_awb_compensation(_red_gain, _blue_gain); */
-/*         } */
-
-/*         default_status(&video_state); */
-/*     } */
-
-/*     void start_stream() { */
-/*         RCLCPP_INFO(this->get_logger(), "Start video encoding..."); */
-/*         res = arducam_set_video_callback(camera_instance, &video_state, video_callback, NULL); */
-/*         if (res) { */
-/*             RCLCPP_INFO( */
-/*                 this->get_logger(), */
-/*                 "Failed to start video encoding, probably due to resolution greater than
- * 1920x1080 " */
-/*                 "or video_state setting error."); */
-/*             throw std::runtime_error("Could not start video stream"); */
-/*         } */
-/*         while (rclcpp::ok()) { */
-/*             usleep(1000 * 300); */
-/*         } */
-/*         RCLCPP_INFO(this->get_logger(), "Stop video encoding..."); */
-/*         arducam_set_video_callback(camera_instance, NULL, NULL, NULL); */
-
-/*         RCLCPP_INFO(this->get_logger(), "Close camera..."); */
-/*         res = arducam_close_camera(camera_instance); */
-/*         if (res) */
-/*             RCLCPP_INFO(this->get_logger(), "close camera status = %d", res); */
-/*     } */
-
-/*   private: */
-/*     void timer_callback() {} */
-/*     int nFrames_ = 0; */
-/*     int _width; */
-/*     int _height; */
-/*     int res; */
-
-/*     int _node_rate_loop; */
-/*     bool _auto_exposure; */
-/*     int _exposure_value; */
-/*     bool _auto_white_balance; */
-/*     bool _auto_wb_compensation; */
-/*     int _red_gain; */
-/*     int _blue_gain; */
-/*     int _scale = 2; */
-/*     std::string frame_id_ = "arducam"; */
-/*     CAMERA_INSTANCE camera_instance; */
-/*     VIDEO_ENCODER_STATE video_state; */
-/*     rclcpp::Publisher<custom_interfaces::msg::H264Image>::SharedPtr publisher_; */
-/* }; */
-
 int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
     rclcpp::Node::SharedPtr arducam = rclcpp::Node::make_shared("arducam");
@@ -226,14 +127,6 @@ int main(int argc, char* argv[]) {
     int _red_gain;
     int _blue_gain;
     int _scale = 2;
-
-    // Available formats:
-    // mode: 7, 1600x600
-    // mode: 8, 2560x720
-    // mode: 9, 3840x1080
-    // mode: 10, 5184x1944
-    // mode: 11, 6528x1848
-    // mode: 12, 6528x2464
 
     arducam->declare_parameter<int>("width", 1920);
     arducam->declare_parameter<int>("height", 1080);
@@ -304,7 +197,9 @@ int main(int argc, char* argv[]) {
         throw std::runtime_error("Could not start video stream");
     }
 
-    RCLCPP_INFO(arducam->get_logger(), "sdfsdfs");
+    while (rclcpp::ok()) {
+        usleep(1000 * 1000 * 10);
+    }
 
     RCLCPP_INFO(arducam->get_logger(), "Stop video encoding...");
     arducam_set_video_callback(camera_instance, NULL, NULL, NULL);
--- src/raspberry_pi/arducam_stereo/src/arducam_stereo_cv.cpp
+++ src/raspberry_pi/arducam_stereo/src/arducam_stereo_cv.cpp
@@ -6,6 +6,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/xphoto/white_balance.hpp>
+#include <rclcpp/executors.hpp>
 #include <rclcpp/node.hpp>
 #include <stdexcept>
 #include <stdio.h>
@@ -24,158 +25,37 @@
 #define VCOS_ALIGN_DOWN(p, n) (((ptrdiff_t)(p)) & ~((n)-1))
 #define VCOS_ALIGN_UP(p, n) VCOS_ALIGN_DOWN((ptrdiff_t)(p) + (n)-1, (n))
 
-rclcpp::Node::SharedPtr arducam_node;
-image_transport::Publisher _pub_image_left;
-image_transport::Publisher _pub_image_right;
+cv::Mat* get_image(CAMERA_INSTANCE camera_instance, int width, int height) {
+    IMAGE_FORMAT fmt = {IMAGE_ENCODING_I420, 50};
+    BUFFER* buffer = arducam_capture(camera_instance, &fmt, 3000);
+    if (!buffer)
+        return NULL;
 
-class ArducamStereo {
-  public:
-    ArducamStereo() {
-        // Available formats: */
-        // mode: 7, 1600x600 */
-        // mode: 8, 2560x720 */
-        // mode: 9, 3840x1080 */
-        // mode: 10, 5184x1944 */
-        // mode: 11, 6528x1848 */
-        // mode: 12, 6528x2464 */
-        arducam_node->declare_parameter<int>("width", 2560);
-        arducam_node->declare_parameter<int>("height", 720);
-        arducam_node->declare_parameter<int>("scale", 2);
-        arducam_node->declare_parameter<bool>("auto_exposure", true);
-        arducam_node->declare_parameter<int>("exposure_value", 5);
-        arducam_node->declare_parameter<bool>("auto_white_balance", true);
-        arducam_node->declare_parameter<bool>("auto_wb_compensation", true);
-        arducam_node->declare_parameter<int>("red_gain", 10);
-        arducam_node->declare_parameter<int>("blue_gain", 50);
+    // The actual width and height of the IMAGE_ENCODING_RAW_BAYER format
+    // and the IMAGE_ENCODING_I420 format are aligned,
+    // width 32 bytes aligned, and height 16 byte aligned.
+    width = VCOS_ALIGN_UP(width, 32);
+    height = VCOS_ALIGN_UP(height, 16);
+    cv::Mat* image = new cv::Mat(cv::Size(width, (int)(height * 1.5)), CV_8UC1, buffer->data);
+    cv::flip(*image, *image, 1);
+    cv::cvtColor(*image, *image, cv::COLOR_YUV2BGR_I420);
+    arducam_release_buffer(buffer);
+    return image;
+}
 
-        arducam_node->get_parameter("width", _width);
-        arducam_node->get_parameter("height", _height);
-        arducam_node->get_parameter("scale", _scale);
-        arducam_node->get_parameter("auto_exposure", _auto_exposure);
-        arducam_node->get_parameter("exposure_value", _exposure_value);
-        arducam_node->get_parameter("auto_white_balance", _auto_white_balance);
-        arducam_node->get_parameter("auto_wb_compensation", _auto_wb_compensation);
-        arducam_node->get_parameter("red_gain", _red_gain);
-        arducam_node->get_parameter("blue_gain", _blue_gain);
+int main(int argc, char* argv[]) {
+    rclcpp::init(argc, argv);
+    image_transport::Publisher _pub_image_left;
+    image_transport::Publisher _pub_image_right;
 
-        RCLCPP_INFO(arducam_node->get_logger(), "Opening camera...");
-        res = arducam_init_camera(&camera_instance);
-        if (res) {
-            RCLCPP_INFO(arducam_node->get_logger(), "init camera status = %d", res);
-            throw std::runtime_error("Could not init camera");
-        }
-        RCLCPP_INFO(arducam_node->get_logger(), "Setting resolution...");
-        res = arducam_set_resolution(camera_instance, &_width, &_height);
-        if (res) {
-            RCLCPP_INFO(arducam_node->get_logger(), "set resolution status = %d", res);
-            throw std::runtime_error("Could not set resolution");
-        } else {
-            RCLCPP_INFO(arducam_node->get_logger(), "current resolution is %dx%d", _width, _height);
-        }
-        if (_auto_exposure) {
-            RCLCPP_INFO(arducam_node->get_logger(), "Enabling Auto Exposure...");
-            arducam_software_auto_exposure(camera_instance, 1);
-        } else {
-            if (arducam_set_control(camera_instance, V4L2_CID_EXPOSURE,
-                                    (int)(_exposure_value * 0xFFFF / 200.0))) {
-                RCLCPP_INFO(arducam_node->get_logger(),
-                            "Failed to set exposure, the camera may not support this control.");
-            }
-        }
-        if (_auto_white_balance) {
-            RCLCPP_INFO(arducam_node->get_logger(), "Enable Auto White Balance...");
-            if (arducam_software_auto_white_balance(camera_instance, 1)) {
-                RCLCPP_INFO(arducam_node->get_logger(), "Automatic white balance not supported");
-            }
-        }
-        if (_auto_wb_compensation) {
-            RCLCPP_INFO(arducam_node->get_logger(), "Enable Auto White Balance Compensation...");
-            arducam_manual_set_awb_compensation(_red_gain, _blue_gain);
-        }
-    }
+    rclcpp::Node::SharedPtr arducam_node = rclcpp::Node::make_shared("arducam");
+    image_transport::ImageTransport it(arducam_node);
+    _pub_image_left = it.advertise("image_left", 1);
+    _pub_image_right = it.advertise("image_right", 1);
 
-    void start_stream() {
-        while (rclcpp::ok()) {
-            if (nFrames_ % 100 == 0) {
-                const int N = 100;
-                int64 t1 = cv::getTickCount();
-                std::cout << "Frames captured: " << cv::format("%5lld", (long long int)nFrames_)
-                          << "    Average FPS: "
-                          << cv::format("%9.1f", (double)cv::getTickFrequency() * N / (t1 - t0))
-                          << "    Average time per frame: "
-                          << cv::format("%9.2f ms",
-                                        (double)(t1 - t0) * 1000.0f / (N * cv::getTickFrequency()))
-                          << "    Average processing time: "
-                          << cv::format("%9.2f ms", (double)(processingTime)*1000.0f /
-                                                        (N * cv::getTickFrequency()))
-                          << std::endl;
-                t0 = t1;
-                processingTime = 0;
-            }
-
-            cv::Mat* image = get_image(camera_instance, _width, _height);
-
-            if (!image)
-                continue;
-            int64 tp0 = cv::getTickCount();
-
-            cv::Mat image_clone = image->clone();
-            cv::Mat image_in;
-            resize(image_clone, image_in,
-                   cv::Size(image_clone.cols / _scale, image_clone.rows / _scale));
-
-            cv::Mat image_left = image_in(cv::Rect(0, 0, image_in.cols / 2, image_in.rows));
-            cv::Mat image_right =
-                image_in(cv::Rect(image_in.cols / 2, 0, image_in.cols / 2, image_in.rows));
-
-            nFrames_ = nFrames_ + 1;
-            sensor_msgs::msg::Image::SharedPtr image_left_out =
-                cv_bridge::CvImage(std_msgs::msg::Header(), "rgb8", image_left).toImageMsg();
-            sensor_msgs::msg::Image::SharedPtr image_right_out =
-                cv_bridge::CvImage(std_msgs::msg::Header(), "rgb8", image_right).toImageMsg();
-
-            image_left_out->header.frame_id = "cam_left";
-            image_left_out->header.stamp = arducam_node->now();
-
-            image_right_out->header.frame_id = "cam_right";
-            image_right_out->header.stamp = arducam_node->now();
-
-            if (_pub_image_left.getNumSubscribers() > 0)
-                _pub_image_left.publish(image_left_out);
-            if (_pub_image_right.getNumSubscribers() > 0)
-                _pub_image_right.publish(image_right_out);
-
-            delete image;
-
-            processingTime += cv::getTickCount() - tp0;
-        }
-
-        RCLCPP_INFO(arducam_node->get_logger(), "Close camera...");
-        res = arducam_close_camera(camera_instance);
-        if (res)
-            RCLCPP_INFO(arducam_node->get_logger(), "close camera status = %d", res);
-    }
-
-  private:
-    cv::Mat* get_image(CAMERA_INSTANCE camera_instance, int width, int height) {
-        IMAGE_FORMAT fmt = {IMAGE_ENCODING_I420, 50};
-        BUFFER* buffer = arducam_capture(camera_instance, &fmt, 3000);
-        if (!buffer)
-            return NULL;
-
-        // The actual width and height of the IMAGE_ENCODING_RAW_BAYER format
-        // and the IMAGE_ENCODING_I420 format are aligned,
-        // width 32 bytes aligned, and height 16 byte aligned.
-        width = VCOS_ALIGN_UP(width, 32);
-        height = VCOS_ALIGN_UP(height, 16);
-        cv::Mat* image = new cv::Mat(cv::Size(width, (int)(height * 1.5)), CV_8UC1, buffer->data);
-        cv::cvtColor(*image, *image, cv::COLOR_YUV2BGR_I420);
-        arducam_release_buffer(buffer);
-        return image;
-    }
     int _width;
     int _height;
-    int nFrames_;
+    int nFrames_ = 0;
     int64 t0 = 0;
     int64 processingTime = 0;
     int res;
@@ -188,14 +68,129 @@ class ArducamStereo {
     int _blue_gain;
     int _scale = 2;
     CAMERA_INSTANCE camera_instance;
-};
 
-int main(int argc, char* argv[]) {
-    rclcpp::init(argc, argv);
-    std::shared_ptr<ArducamStereo> arducam = std::make_shared<ArducamStereo>();
-    arducam_node = rclcpp::Node::make_shared("arducam");
-    image_transport::ImageTransport it(arducam_node);
-    arducam->start_stream();
+    // Available formats: */
+    // mode: 7, 1600x600 */
+    // mode: 8, 2560x720 */
+    // mode: 9, 3840x1080 */
+    // mode: 10, 5184x1944 */
+    // mode: 11, 6528x1848 */
+    // mode: 12, 6528x2464 */
+    arducam_node->declare_parameter<int>("width", 2560);
+    arducam_node->declare_parameter<int>("height", 720);
+    arducam_node->declare_parameter<int>("scale", 2);
+    arducam_node->declare_parameter<bool>("auto_exposure", true);
+    arducam_node->declare_parameter<int>("exposure_value", 5);
+    arducam_node->declare_parameter<bool>("auto_white_balance", true);
+    arducam_node->declare_parameter<bool>("auto_wb_compensation", true);
+    arducam_node->declare_parameter<int>("red_gain", 10);
+    arducam_node->declare_parameter<int>("blue_gain", 50);
+
+    arducam_node->get_parameter("width", _width);
+    arducam_node->get_parameter("height", _height);
+    arducam_node->get_parameter("scale", _scale);
+    arducam_node->get_parameter("auto_exposure", _auto_exposure);
+    arducam_node->get_parameter("exposure_value", _exposure_value);
+    arducam_node->get_parameter("auto_white_balance", _auto_white_balance);
+    arducam_node->get_parameter("auto_wb_compensation", _auto_wb_compensation);
+    arducam_node->get_parameter("red_gain", _red_gain);
+    arducam_node->get_parameter("blue_gain", _blue_gain);
+
+    RCLCPP_INFO(arducam_node->get_logger(), "Opening camera...");
+    res = arducam_init_camera(&camera_instance);
+    if (res) {
+        RCLCPP_INFO(arducam_node->get_logger(), "init camera status = %d", res);
+        throw std::runtime_error("Could not init camera");
+    }
+    RCLCPP_INFO(arducam_node->get_logger(), "Setting resolution...");
+    res = arducam_set_resolution(camera_instance, &_width, &_height);
+    if (res) {
+        RCLCPP_INFO(arducam_node->get_logger(), "set resolution status = %d", res);
+        throw std::runtime_error("Could not set resolution");
+    } else {
+        RCLCPP_INFO(arducam_node->get_logger(), "current resolution is %dx%d", _width, _height);
+    }
+    if (_auto_exposure) {
+        RCLCPP_INFO(arducam_node->get_logger(), "Enabling Auto Exposure...");
+        arducam_software_auto_exposure(camera_instance, 1);
+    } else {
+        if (arducam_set_control(camera_instance, V4L2_CID_EXPOSURE,
+                                (int)(_exposure_value * 0xFFFF / 200.0))) {
+            RCLCPP_INFO(arducam_node->get_logger(),
+                        "Failed to set exposure, the camera may not support this control.");
+        }
+    }
+    if (_auto_white_balance) {
+        RCLCPP_INFO(arducam_node->get_logger(), "Enable Auto White Balance...");
+        if (arducam_software_auto_white_balance(camera_instance, 1)) {
+            RCLCPP_INFO(arducam_node->get_logger(), "Automatic white balance not supported");
+        }
+    }
+    if (_auto_wb_compensation) {
+        RCLCPP_INFO(arducam_node->get_logger(), "Enable Auto White Balance Compensation...");
+        arducam_manual_set_awb_compensation(_red_gain, _blue_gain);
+    }
+
+    while (rclcpp::ok()) {
+        if (nFrames_ % 100 == 0) {
+            const int N = 100;
+            int64 t1 = cv::getTickCount();
+            std::cout << "Frames captured: " << cv::format("%5lld", (long long int)nFrames_)
+                      << "    Average FPS: "
+                      << cv::format("%9.1f", (double)cv::getTickFrequency() * N / (t1 - t0))
+                      << "    Average time per frame: "
+                      << cv::format("%9.2f ms",
+                                    (double)(t1 - t0) * 1000.0f / (N * cv::getTickFrequency()))
+                      << "    Average processing time: "
+                      << cv::format("%9.2f ms",
+                                    (double)(processingTime)*1000.0f / (N * cv::getTickFrequency()))
+                      << std::endl;
+            t0 = t1;
+            processingTime = 0;
+        }
+
+        cv::Mat* image = get_image(camera_instance, _width, _height);
+
+        if (!image)
+            continue;
+        int64 tp0 = cv::getTickCount();
+
+        cv::Mat image_clone = image->clone();
+        cv::Mat image_in;
+        resize(image_clone, image_in,
+               cv::Size(image_clone.cols / _scale, image_clone.rows / _scale));
+
+        cv::Mat image_left = image_in(cv::Rect(0, 0, image_in.cols / 2, image_in.rows));
+        cv::Mat image_right =
+            image_in(cv::Rect(image_in.cols / 2, 0, image_in.cols / 2, image_in.rows));
+
+        nFrames_ = nFrames_ + 1;
+        sensor_msgs::msg::Image::SharedPtr image_left_out =
+            cv_bridge::CvImage(std_msgs::msg::Header(), "rgb8", image_left).toImageMsg();
+        sensor_msgs::msg::Image::SharedPtr image_right_out =
+            cv_bridge::CvImage(std_msgs::msg::Header(), "rgb8", image_right).toImageMsg();
+
+        image_left_out->header.frame_id = "cam_left";
+        image_left_out->header.stamp = arducam_node->now();
+
+        image_right_out->header.frame_id = "cam_right";
+        image_right_out->header.stamp = arducam_node->now();
+
+        if (_pub_image_left.getNumSubscribers() > 0)
+            _pub_image_left.publish(image_left_out);
+        if (_pub_image_right.getNumSubscribers() > 0)
+            _pub_image_right.publish(image_right_out);
+
+        delete image;
+
+        processingTime += cv::getTickCount() - tp0;
+    }
+
+    RCLCPP_INFO(arducam_node->get_logger(), "Close camera...");
+    res = arducam_close_camera(camera_instance);
+    if (res)
+        RCLCPP_INFO(arducam_node->get_logger(), "close camera status = %d", res);
+
     rclcpp::shutdown();
     return 0;
 }
